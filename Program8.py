008f9b8397c8f45534cfb3ea94540d4c2e061d80
B14ZE07 <akhil.2142007@gmail.com>
2023-05-04 10:52:11 +0400

    4th may 2023

---
+++ Program8.py
@@ -0,0 +1,17 @@
+#0,1,1,2,3,5,8,13,21,34
+n = int(input("Max number of elements in series: "))
+a = 0
+b = 1
+c = 0
+print(a)
+print(b)
+for i in range(n - 2):
+    c = a + b
+    print(c)
+    a = b
+    b = c
+print('')
+print("Here is your sequence of", n, "numbers :)")
+
+    
+    
