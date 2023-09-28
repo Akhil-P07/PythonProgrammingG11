fbb0d1815e3c63aeb0e7dcc7b889b1207537f978
B14ZE07 <akhil.2142007@gmail.com>
2023-09-28 23:06:05 +0400

    Fixed syntax error

--- Program6.py
+++ Program6.py
@@ -1,7 +1,7 @@
 #Checks if alphabet is lower or uppercase
 alpha = input("enter alphabet = ") 
 
-if alpha == alpha.:
+if alpha == alpha.upper():
     print("Given alphabet is Uppercase")
 else:
     print("Given alphabet is Lowercase")
