284fe03c3a5d2b3704ea698c871c0dda65a2aa54
B14ZE07 <akhil.2142007@gmail.com>
2023-09-29 00:10:28 +0400

    Fixed semantic errors

--- Program8.py
+++ Program8.py
@@ -1,11 +1,21 @@
 #Prints the fibonacci sequence until the last set number eg. 0,1,1,2,3,5,8,13
-max = int(input("Enter the largest no. in the fibonacci sequence: "))
+max = int(input("Enter largest no. in sequence: ")) 
 a = 0
 b = 1
-while (b < max):
-    print(a,b,end=" ")
+seq=list()
+for i in range(max):
+    seq.append(a)
+    seq.append(b)
+    if (a > max):
+        max = a
+        break
+    elif (b > max):
+        max = b
+        break
     a += b
     b += a
-
+loc = seq.index(max)
+for i in seq[:loc]:
+    print(i, end =" ")
 
     
