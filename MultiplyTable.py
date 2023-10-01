70fa8178171a7a9eb17d4db4896cb19b4027e3ec
B14ZE07 <akhil.2142007@gmail.com>
2023-10-01 18:59:34 +0400

    Changed Filenames

--- MultiplyTable.py
+++ MultiplyTable.py
@@ -1,4 +1,4 @@
 num = int(input("Enter num for table: ")) #Finds multiplication table of this num
-r = int(input("Enter largest multiple of given numm: "))
+r = int(input("Enter largest multiple of given num: "))
 for i in range(1, r + 1):
     print(num, "X", i, "=", num * i)
