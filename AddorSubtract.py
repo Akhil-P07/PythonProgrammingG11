4c379fabef83c3c6e88b46aee564597cd5781abe
B14ZE07 <akhil.2142007@gmail.com>
2023-10-01 19:22:00 +0400

    Fixed logical operator error

--- AddorSubtract.py
+++ AddorSubtract.py
@@ -1,10 +1,11 @@
 #Addition and subtraction only
-num1 = int(input("Enter 1stNum: "))
+print("Enter numbers to do operation then choose + or -\n")
+num1 = int(input("Enter 1st Num: "))
 num2 = int(input("Enter 2nd Num: "))
 oper = input("Enter operation: ")
-if (oper == "-" or "subtract"):
-    print(num1 - num2)
-elif(oper == "+" or "add"):
-    print(num1 + num2)
+if (oper == "-"):
+    print("result: ",num1 - num2)
+elif(oper == "+"):
+    print("result: ",num1 + num2)
 else:
     print("error")
